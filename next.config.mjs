89fdd76f4cc537cc230ea245033a2b1297bef846
0x7d202060618 <tymothycromer1@outlook.com>
2024-09-03 04:30:47 -0400

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 import withExportImages from "next-export-optimize-images";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
 };
 
 export default withExportImages(nextConfig);
